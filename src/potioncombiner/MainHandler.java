793acdde09824f5c801319cb397dc5d96b173ba0
igormec <igor_mec@hotmail.com>
2015-04-10 06:46:41 -0400

    First Test

--- src/potioncombiner/MainHandler.java
+++ src/potioncombiner/MainHandler.java
@@ -3,15 +3,13 @@ package potioncombiner;
 //import org.tbot.client.GameObject;
 import org.tbot.internal.AbstractScript;
 import org.tbot.internal.handlers.LogHandler;
-import org.tbot.methods.Bank;
-import org.tbot.methods.GameObjects;
-import org.tbot.methods.Random;
-import org.tbot.methods.Time;
+import org.tbot.methods.*;
 import org.tbot.methods.tabs.Inventory;
 import org.tbot.methods.walking.Path;
 import org.tbot.methods.walking.Walking;
 import org.tbot.util.Condition;
 import org.tbot.wrappers.GameObject;
+import org.tbot.wrappers.Item;
 
 /**
  * Created by Igor on 010. Apr 10, 15.
@@ -22,6 +20,42 @@ public class MainHandler extends AbstractScript {
         LogHandler.log("Potion Combiner started");
         return true;
     }
+
+    public void withdrawPots(String potType, int amount){
+        //WITHDRAW CODE
+
+        if(Bank.contains(potType)) {            //IF Energy Potion(3) Found
+            Bank.withdraw(potType, amount);     //Withdraw All
+            Bank.close();                       //Close bank screen
+        }else if(!Bank.contains(potType)){
+            LogHandler.log("No energy potions, stopping script.");  //PRINT "No energy potions"
+            //return -1;//STOP SCRIPT
+        }else {
+            LogHandler.log("Unknown error, stopping script.");  //PRINT "No energy potions"
+            //return -1;//STOP SCRIPT
+        }
+    }
+
+    //Uses slot grabbing (Rewrite for more modular design)
+    //Requires Full inventory of potions
+    public void combinePots(){
+        //String[] pots = {"", "Energy potion(1)", "Energy potion(2)", "Energy potion(3)", "Energy potion(4)"};
+        Item potToEmpty, potToFill;
+        for(int x=0; x<=24; x+=4){
+            potToEmpty = Inventory.getInSlot(x);
+            for(int y=0; y<3; y++){
+                potToFill = Inventory.getInSlot(x+y+1);
+                Inventory.useItemOn(potToEmpty, potToFill);
+                Time.sleepUntil(new Condition() {
+                    @Override
+                    public boolean check(){
+                        return Players.getLocal().getAnimation() == -1;
+                    }
+                }, Random.nextInt(1400,1500));
+            }
+        }
+    }
+
 
 
     @Override
@@ -41,34 +75,54 @@ public class MainHandler extends AbstractScript {
                     );
                 }
             }
-            String pot3 = "Energy potion(3)";
-            if(Bank.contains(pot3)) {         //IF Energy Potion(3) Found
-                Bank.withdraw(pot3, 28);      //Withdraw All
-                Bank.close();                 //Close bank screen
-            }else if(!Bank.contains(pot3)){
-                LogHandler.log("No energy potions, stopping script.");  //PRINT "No energy potions"
-                return -1;//STOP SCRIPT
-            }else{
-                LogHandler.log("Unknown error, stopping script.");  //PRINT "No energy potions"
-                return -1;//STOP SCRIPT
+
+            withdrawPots("Energy potion(3)",28);
+            combinePots();
+
+
+
 
 
         }
         else if(!(Bank.isOpen())){
             final GameObject bankBooth = GameObjects.getNearest("Bank booth");
-            if(bankBooth.distance() > 8){
-                Path pathToBooth = Walking.findPath(bankBooth);
-                if(pathToBooth != null){
-                    pathToBooth.traverse();
-                    Time.sleepUntil(new Condition() {
-                                        @Override
-                                        public boolean check() {
-                                            return bankBooth.distance() < 8 || bankBooth.isOnScreen();
-                                        }
-                                    },
-                            Random.nextInt(900, 1100)
-                    );
+
+            if (bankBooth != null && bankBooth.isOnScreen()) {
+                if (Players.getLocal().getAnimation() == -1) {
+                    if (bankBooth.interact("Bank")) {
+                        Time.sleepUntil(new Condition() {
+                                            @Override
+                                            public boolean check() {
+                                                return Bank.isOpen();
+                                            }
+                                        },
+                                Random.nextInt(1200, 1600)
+                        );
+                    }
                 }
+            }
+            else if (bankBooth != null && !(bankBooth.isOnScreen())) {
+                if (bankBooth.distance() > 6) {
+                    Path pathToBooth = Walking.findPath(bankBooth);
+                    if (pathToBooth != null) {
+                        pathToBooth.traverse();
+                        Time.sleepUntil(new Condition() {
+                                            @Override
+                                            public boolean check() {
+                                                return bankBooth.distance() < 6 || bankBooth.isOnScreen();
+                                            }
+                                        },
+                                Random.nextInt(900, 1150)
+                        );
+                    }
+                } else if (bankBooth.distance() <= 6) {
+                    Camera.turnTo(bankBooth);
+                    Time.sleep(300, 420);
+                }
+            }
+            else if (bankBooth == null) {
+
+                LogHandler.log("bankBooth is null");
             }
         }
 
